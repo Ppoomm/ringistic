5f7cd8713955fdc1df0f53a9897d64e237a5c09b
PPoomm <pongpoom.poom16@mail.kmutt.ac.th>
2021-09-15 18:51:20 +0700

    dashboardPage

---
+++ src/Components/dashboardPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import wedding from "../assets/images/wedding.jpg"
+import dress from "../assets/images/dress.png"
+import engagement from  "../assets/images/engagement.jpg"
+import diamond from "../assets/images/diamond.png"
+
+const DashboardPage = () =>{
+    return (
+        <div className="wedding">
+            <div> <img src={wedding} alt="wedding"/></div>
+            
+        </div>
+    )
+}
+export default DashboardPage;
