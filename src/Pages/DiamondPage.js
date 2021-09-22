a12951210fb91591a99ea31176b3c268e2348e83
PPoomm <pongpoom.poom16@mail.kmutt.ac.th>
2021-09-23 00:50:30 +0700

    edit dia en pro js

---
+++ src/Pages/DiamondPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+const DiamondPage = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default DiamondPage
---
+++ src/Pages/EngagementPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+const EngagementPage = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default EngagementPage
