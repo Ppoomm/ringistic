eb6ad137272a93220262d0a4b3b28f916f5e8280
PPoomm <pongpoom.poom16@mail.kmutt.ac.th>
2021-09-23 00:51:03 +0700

    edit wedd

---
+++ src/Pages/WeddingPage.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import wedding from "../assets/images/wedding.png";
+import dress from "../assets/images/dress.png";
+import engagement from "../assets/images/engagement.png";
+import diamond from "../assets/images/diamond.png";
+import { Container, Grid, Paper, Typography } from "@material-ui/core";
+import Sidebar from "../Components/Sidebar";
+import Header from "../Components/Header";
+
+const WeddingPage = () => {
+  return (
+    <div>
+      <Header/>
+
+    </div>
+    
+
+  );
+};
+export default WeddingPage;
