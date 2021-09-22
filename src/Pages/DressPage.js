dce3006b66b42263d46103fed5d6ff5395add394
PPoomm <pongpoom.poom16@mail.kmutt.ac.th>
2021-09-23 00:50:50 +0700

    edit dresspage

---
+++ src/Pages/DressPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+const DressPage = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default DressPage
