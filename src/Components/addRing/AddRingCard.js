88c69b18bc4f8099c7f7a3415db35350c0af00d7
PPoomm <pongpoom.poom16@mail.kmutt.ac.th>
2021-09-23 00:42:25 +0700

    add ring card

---
+++ src/Components/addRing/AddRingCard.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./AddRingCard.css";
+import { Grid, Box, Typography } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import { IconButton } from "@material-ui/core";
+
+const AddRingCard = ({ status }) => {
+  return (
+    <div className="add-ring__card">
+      <Grid container>
+        <Grid item md={3}>
+          <Box display="flex" justifyContent="center" flexDirection="column">
+            <div className="card-heading">Wedding Rings</div>
+            <div className="card-subheading">Post on 05/21/2021</div>
+          </Box>
+        </Grid>
+        <Grid item md={3}>
+          <Box display="flex" alignItems="center" className="box-full__height">
+            <div
+              className={`ring-status ring-status__${
+                status === "Published"
+                  ? "green"
+                  : status === "Deleted"
+                  ? "red"
+                  : "yellow"
+              }`}
+            >
+              {status}
+            </div>
+          </Box>
+        </Grid>
+        <Grid item md={3}>
+          <Box display="flex" alignItems="center" className="box-full__height">
+            <div className="card-heading">Out of Stock</div>
+          </Box>
+        </Grid>
+        <Grid item md={3}>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+            <IconButton>
+              <DeleteOutlineIcon />
+            </IconButton>
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default AddRingCard;
