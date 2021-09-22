bbc50e9b0c838eb31180d02630cb3609884b2ade
PPoomm <pongpoom.poom16@mail.kmutt.ac.th>
2021-09-23 00:33:47 +0700

    edit app js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Route, Switch } from "react-router-dom";
+import { Link,useLocation } from "react-router-dom";
 import Sidebar from "./Components/Sidebar";
-import AddRingPage from './Components/AddRingPage';
-import React from 'react';
-import Profile from './Components/ProfilePage';
-import DashboardPage from './Components/dashboardPage';
-import StatisticsPage from './Components/StatisticsPage';
+import AddRingPage from "./Pages/AddRingPage";
+import Profile from "./Pages/ProfilePage";
+import DashboardPage from "./Pages/dashboardPage";
+import ProfilePage from "./Pages/ProfilePage";
+import WeddingPage from "./Pages/WeddingPage";
+import EngagementPage from "./Pages/EngagementPage";
+import DressPage from "./Pages/DressPage";
+import DiamondPage from "./Pages/DiamondPage";
+import logo from "./assets/images/logo.png";
 
+const App = () => {
 
-const App=()=>{
-
-    return(
-      <div>
-      <div className ="App">
-        <Sidebar/>
+  return (
+    <div className="App">
+      <Sidebar/>
+      <div className="content">
         <Switch>
-          <Route path="/" component={DashboardPage} exact/>
-          <Route path="/ring/add" component ={AddRingPage}/>
-          <Route path="/profile" component={Profile}/>
-          <Route path="/Statistics" component={StatisticsPage}/>
+          <Route path="/" component={DashboardPage} exact />
+          <Route path="/ring/add" component={AddRingPage} exact />
+          <Route path="/profile" component={ProfilePage} exact />
+          <Route path="/wedding/detail" component={WeddingPage} exact />
+          <Route path="/engagement/detail" component={EngagementPage} exact />
+          <Route path="/dress/detail" component={DressPage} exact />
+          <Route path="/diamond/detail" component={DiamondPage} exact />
         </Switch>
       </div>
-      </div>
-    );
-
-  
-}
-
-  
+    </div>
+  );
+};
 
 export default App;
