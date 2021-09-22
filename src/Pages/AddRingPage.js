888fd867121273c18f306af6ad4f78b5b2082a33
PPoomm <pongpoom.poom16@mail.kmutt.ac.th>
2021-09-23 00:49:13 +0700

    new add ring page

---
+++ src/Pages/AddRingPage.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import "../AddRingPage.css";
+import "../App.css";
+import HeadTable from "../Components/HeadTable";
+import { makeStyles } from "@material-ui/styles";
+import AddRingCard from '../Components/addRing/AddRingCard'
+
+const useStyles = makeStyles({
+  box: {
+    padding: "36px"
+  }
+});
+
+function AddRingPage() {
+    const classes = useStyles();
+  return (
+    <div className={classes.box}>
+      <HeadTable />
+      <AddRingCard status="Published"/>
+      <AddRingCard status="Pending"/>
+      <AddRingCard status="Deleted"/>
+      <AddRingCard status="Published"/>
+      <AddRingCard status="Published"/>
+      <AddRingCard status="Published"/>
+    </div>
+  );
+}
+
+export default AddRingPage;
