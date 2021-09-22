3f0e696a425c840814405a5797cad70f838e6b13
PPoomm <pongpoom.poom16@mail.kmutt.ac.th>
2021-09-23 00:45:55 +0700

    edit sidebar, sidebar data

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,40 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../App.css";
 import { SidebarData } from "./SidebarData";
 import logo from "../assets/images/logo.png";
 import "../Images.css";
+import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
 
+const Sidebar = () => {
+  const location = useLocation();
 
-
-function Sidebar() {
   return (
-
     <div className="Sidebar">
-      <ul className="SidebarList">
-        <div className="logo-box"> 
-        <div className = "logo">
-          <img src={logo} alt="logo"/>
+        <div className="logo-box">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
         </div>
+        <div  className="icon-column">
+          <div>
+            {SidebarData.map((val, key) => {
+              return (
+                <Link
+                  key={key}
+                  className="row"
+                  to={val.Link}
+                  id={location.pathname === val.Link ? "active" : ""}
+                >
+                  <div id="icon">{val.Icon}</div>{" "}
+                  <div id="title"> {val.title}</div>
+                </Link>
+              );
+            })}
+          </div>
+
+          <Link
+            className="row"
+            to={"/Logout"}
+            id={location.pathname === "/Logout" ? "active" : ""}
+          >
+            <div id="icon">
+              <MeetingRoomIcon />
+            </div>
+            <div id="title"> Logout</div>
+          </Link>
         </div>
-        {SidebarData.map((val, key) => {
-          return (
-            <Link
-              key={key}
-              className="row"
-              id={window.location.pathname === val.Link ? "active" : ""}
-              to={val.Link}
-            >
-              <div id="icon">{val.Icon}</div> <div id="title"> {val.title}</div>
-            </Link>
-          );
-        })}
-      </ul>
-      
     </div>
-    
-    
   );
-}
+};
 
 export default Sidebar;
--- src/Components/SidebarData.js
+++ src/Components/SidebarData.js
@@ -1,40 +1,30 @@
-
-import React from 'react'
-import HomeIcon from '@material-ui/icons/Home';
-import AddIcon from '@material-ui/icons/Add';
-import EqualizerIcon from '@material-ui/icons/Equalizer';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
-
+import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import AddIcon from "@material-ui/icons/Add";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
 
 export const SidebarData = [
-    
-    {
-        title: "Dashboard",
-        Icon: <HomeIcon/>,
-        Link:"/"
-    },
-    {
-        title: "Add Ring",
-        Icon: <AddIcon/>,
-        Link:"/ring/add"
-    },
-    {
-        title: "Statistics",
-        Icon: <EqualizerIcon/>,
-        Link:"/Statistics"
-    },
-    {
-        title: "Profile",
-        Icon: <AccountCircleIcon/>,
-        Link:"/Profile"
-    },
-    {
-    title: "Logout",
-    Icon: <MeetingRoomIcon/>,
-    Link:"/Logout"
-    },
-    
+  {
+    title: "Dashboard",
+    Icon: <HomeIcon />,
+    Link: "/",
+  },
+  {
+    title: "Add Ring",
+    Icon: <AddIcon />,
+    Link: "/ring/add",
+  },
+  // {
+  //     title: "Statistics",
+  //     Icon: <EqualizerIcon/>,
+  //     Link:"/Statistics"
+  // },
+  {
+    title: "Profile",
+    Icon: <AccountCircleIcon />,
+    Link: "/Profile",
+  },
 
-]
- 
+];
