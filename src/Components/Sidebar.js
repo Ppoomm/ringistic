c625a272521f2aa7f9d9b1ce44773e1a6802c5f8
Tawan <richmanother@gmail.com>
2021-11-13 19:44:04 +0700

    add logout button and can logout
    add logout button and can logout

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,48 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../App.css";
 import { SidebarData } from "./SidebarData";
 import logo from "../assets/images/logo.png";
 import "../Images.css";
 import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
-import { Button } from "@material-ui/core";
-import auth from "../firebase"
+import { Button, Grid } from "@material-ui/core";
+import auth from "../firebase";
 const Sidebar = () => {
   const location = useLocation();
-  
+  const [session, setSession] = useState({
+    isLoggedIn: true,
+    currentUser: null,
+    errorMessage: null,
+  });
   return (
     <div className="Sidebar">
-      <div className="logo-box">
-        <div className="logo">
-          <img src={logo} alt="logo" />
+      <div>
+        <div className="logo-box">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+        <div className="icon-column">
+          <div>
+            {SidebarData.map((val, key) => {
+              return (
+                <Link
+                  key={key}
+                  className="row"
+                  to={val.Link}
+                  id={location.pathname === val.Link ? "active" : ""}
+                >
+                  <div id="icon">{val.Icon}</div>{" "}
+                  <div id="title"> {val.title}</div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
-      <div className="icon-column">
-        <div>
-          {SidebarData.map((val, key) => {
-            return (
-              <Link
-                key={key}
-                className="row"
-                to={val.Link}
-                id={location.pathname === val.Link ? "active" : ""}
-              >
-                <div id="icon">{val.Icon}</div>{" "}
-                <div id="title"> {val.title}</div>
-              </Link>
-            );
-          })}
-        </div>
 
+      <div >
         <Button
           style={{
-            backgroundColor: "#ff8989",
+            backgroundColor: "#15012E",
             color: "white",
             width: "100%",
-            borderRadius: "30px",
             padding: "12px 0px",
+            bottom: "0px",
           }}
-          // onClick={handleLogout}
+          onClick={() => {
+            auth.signOut().then((response) => {
+              setSession({
+                isLoggedIn: false,
+                currentUser: null,
+              });
+            }, window.location.reload(false));
+          }}
         >
           Logout
         </Button>
